d0bf8439cb6e7720afd6551a702ed4e74f32b670
JEONG-HO-JAE <98eddiechung@gmail.com>
2024-11-27 04:04:54 +0000

    delete archived

--- Diffusion-Based/models/types_.py
+++
@@ -1,3 +0,0 @@
-from typing import  TypeVar, Callable, Iterator, Tuple, Optional, Type, Union, List, ClassVar
-
-Tensor = TypeVar('torch.tensor')
--- Diffusion-Based/variance_scheduler/__init__.py
+++
@@ -1,6 +0,0 @@
-from .abs_var_scheduler import Scheduler
-from .cosine import CosineScheduler
-from .hyperbolic_secant import HyperbolicSecant
-from .linear import LinearScheduler
-
-__all__ = [Scheduler, CosineScheduler, HyperbolicSecant, LinearScheduler]
