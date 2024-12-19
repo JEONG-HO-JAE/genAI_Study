ead38c84e505177b9c3c19f25d0ef501f46aec78
JEONG-HO-JAE <98eddiechung@gmail.com>
2024-12-19 02:14:25 +0000

    edit shell scripts

--- Diffusion/sample.sh
+++ Diffusion/sample.sh
@@ -6,10 +6,6 @@ echo "DATA_DIR: $DATA_DIR"
 
 export LOGDIR
 EMA_FILE=$(find "$LOGDIR" -type f -name "model*.pt" | tail -n 1)
-# EMA_FILE=$(find "$LOGDIR" -type f -name "model005000.pt")
 echo "EMA_FILE: $EMA_FILE"
 
-python scripts/image_sample.py $DATA_DIR --model_path $EMA_FILE $MODEL_FLAGS $SAMPLE_FLAGS 
-
-# python scripts/image_sample.py --model_path $EMA_FILE --use_ddim True $MODEL_FLAGS $DIFFUSION_FLAGS
-# python scripts/image_sample.py --model_path $EMA_FILE $MODEL_FLAGS $DIFFUSION_FLAGS
+python scripts/image_sample.py $DATA_DIR --model_path $EMA_FILE $MODEL_FLAGS $SAMPLE_FLAGS
--- Diffusion/train.sh
+++ Diffusion/train.sh
@@ -7,6 +7,10 @@ echo "DIFFUSION_FLAGS: $DIFFUSION_FLAGS"
 echo "TRAIN_FLAGS: $TRAIN_FLAGS"
 echo "DATA_DIR: $DATA_DIR"
 export LOGDIR
+mkdir $LOGDIR
+# LOGDIR 아래에 config.env 복사
+cp config.env "$LOGDIR/config.env"
+echo "config.env saved to $LOGDIR"
 
 # 명령 실행
 python scripts/image_train.py $DATA_DIR $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
